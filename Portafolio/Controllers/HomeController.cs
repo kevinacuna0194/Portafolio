e4a0c914ba09c12e9eb2627a03e996b44014f2ea
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-03-08 19:45:11 -0300

    Principio de Responsabilidad Única

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 
 namespace Portafolio.Controllers;
 
@@ -15,48 +16,14 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        var proyectos = ObtenerProyectos().Take(3).ToList();
+        var RepositorioProyectos = new RepositorioProyectos();
+        var proyectos = RepositorioProyectos.ObtenerProyectos().Take(3).ToList();
         var model = new HomeIndexViewModel
         {
             Proyectos = proyectos
         };
 
         return View(model);
-    }
-
-    private List<Proyecto> ObtenerProyectos()
-    {
-        return new List<Proyecto>()
-        {
-            new Proyecto
-            {
-                Titulo = "Amazon",
-                Descripcion = "E-Commerce realizado en ASP.NET Core",
-                ImagenURL = "/imagenes/amazon.png",
-                Link = "https://www.amazon.com"
-            },
-            new Proyecto
-            {
-                Titulo = "New York Times",
-                Descripcion = "Página de noticias en React",
-                ImagenURL = "/imagenes/nyt.png",
-                Link = "https://www.nytimes.com"
-            },
-            new Proyecto
-            {
-                Titulo = "Reddit",
-                Descripcion = "Red social para compartir en comunidades",
-                ImagenURL = "/imagenes/reddit.png",
-                Link = "https://www.reddit.com"
-            },
-            new Proyecto
-            {
-                Titulo = "Steam",
-                Descripcion = "Tienda en línea para comprar videojuegos",
-                ImagenURL = "/imagenes/steam.png",
-                Link = "https://store.steampowered.com/"
-            }
-        };
     }
 
     public IActionResult Privacy()
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,42 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+        {
+            new Proyecto
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-Commerce realizado en ASP.NET Core",
+                ImagenURL = "/imagenes/amazon.png",
+                Link = "https://www.amazon.com"
+            },
+            new Proyecto
+            {
+                Titulo = "New York Times",
+                Descripcion = "Página de noticias en React",
+                ImagenURL = "/imagenes/nyt.png",
+                Link = "https://www.nytimes.com"
+            },
+            new Proyecto
+            {
+                Titulo = "Reddit",
+                Descripcion = "Red social para compartir en comunidades",
+                ImagenURL = "/imagenes/reddit.png",
+                Link = "https://www.reddit.com"
+            },
+            new Proyecto
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en línea para comprar videojuegos",
+                ImagenURL = "/imagenes/steam.png",
+                Link = "https://store.steampowered.com/"
+            }
+        };
+        }
+    }
+}
